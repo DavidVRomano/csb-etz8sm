0cd444e148269789ea36441d572600aa668868c2
Code Sandbox <hello@codesandbox.io>
2022-05-17 06:25:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8a42b9fd98ba23a56ae3aa0933b20a47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e967575bccf5dd0d7352774dc85db0a",
+    "url": "https://davidvromano.github.io/csb-etz8sm/index.html"
+  },
+  {
+    "revision": "c54655b483f1bb92302b",
+    "url": "https://davidvromano.github.io/csb-etz8sm/static/js/2.59cded49.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://davidvromano.github.io/csb-etz8sm/static/js/2.59cded49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ca2ab507984f8185b4",
+    "url": "https://davidvromano.github.io/csb-etz8sm/static/js/main.5d56ff63.chunk.js"
+  },
+  {
+    "revision": "9fc1b96503a10bb3b079",
+    "url": "https://davidvromano.github.io/csb-etz8sm/static/js/runtime-main.9c7e10bc.js"
+  }
+]);
